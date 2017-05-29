e6a810248cb251404bc3a092b164fe5699c5d2d2
Briceida <BriceidaMariscal@gmail.com>
2017-05-29 12:37:54 -0700

    Finished course

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-# 
-# tournament.py -- implementation of a Swiss-system tournament
+#
+# players.py -- implementation of a Swiss-system players
 #
 
 import psycopg2
@@ -13,25 +13,50 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM matches;")
+    conn.commit()
+    conn.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM players;")
+    conn.commit()
+    conn.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
-
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT count(*) FROM players;")
+    numPlayers = cursor.fetchone()
+    totalPlayers = int(numPlayers[0])
+    print(totalPlayers)
+    conn.commit()
+    conn.close()
+    return totalPlayers
+    #return numPlayers[0][0]
 
 def registerPlayer(name):
-    """Adds a player to the tournament database.
-  
+    """Adds a player to the players database.
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    conn = connect()
+    cursor = conn.cursor()
+    param = (name,)
+    cursor.execute("INSERT INTO players (name) VALUES (%s);", param)
+    conn.commit()
+    conn.close()
 
 
 def playerStandings():
@@ -47,6 +72,18 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute('''SELECT id, name,
+        (SELECT count(*) FROM matches WHERE players.id = matches.winner)
+        AS wins,
+        (SELECT count(*) FROM matches WHERE players.id = matches.winner OR players.id = matches.loser)
+        AS matches
+        FROM players
+        ORDER BY wins DESC''')
+    listTuples = cursor.fetchall()
+    conn.close()
+    return listTuples
 
 
 def reportMatch(winner, loser):
@@ -56,22 +93,38 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO matches (winner, loser) values (%s, %s)", (winner, loser,))
+    conn.commit()
+    conn.close()
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
         name1: the first player's name
         id2: the second player's unique id
         name2: the second player's name
-    """
+                 """
+
+    playerStandingsList = playerStandings()
+    listofTuples = []
+    if len(playerStandingsList) % 2 == 0:
+        for i in range(0, len(playerStandingsList), 2):
+            listofTuples.append((playerStandingsList[i][0], playerStandingsList[i][1],
+            playerStandingsList[i+1][0], playerStandingsList[i+1][1]))
+        return listofTuples
+    else:
+        return []
+
 
 
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,9 +1,21 @@
 -- Table definitions for the tournament project.
---
+--      id: id of players added, it's automatically created via the serial type
+--      name: name of players, text type
+--      wins: numbers of wins, integer type
+--      matches: number of matches played, integer
+
 -- Put your SQL 'create table' statements in this file; also 'create view'
 -- statements if you choose to use it.
 --
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+\c tournament;
 
+CREATE TABLE players(id SERIAL PRIMARY KEY,
+                          name TEXT);
 
+CREATE TABLE matches (
+    id SERIAL PRIMARY KEY,
+    winner INTEGER REFERENCES players(id),
+    loser INTEGER REFERENCES players(id)
+);
